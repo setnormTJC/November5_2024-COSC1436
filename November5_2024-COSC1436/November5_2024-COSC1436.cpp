e3226fc76e38b269dbd17060eada4f605ccdfab4
Work <Work@MSI>
2024-11-05 10:20:32 -0600

    Add project files.

---
+++ November5_2024-COSC1436/November5_2024-COSC1436.cpp
@@ -0,0 +1,30 @@
+// November5_2024-COSC1436.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using std::cout; 
+using std::string; 
+
+
+int main()
+{
+	//our first (sort of) array: 
+	//this is a "C-style", static array
+	string groceryList[5] =
+	{
+		"Eggs",
+		"bacon",
+		"tuna",
+	};
+
+	//cout << "What is this? " << groceryList << "\n";
+
+	cout << "GroceryList[0] yields: " << groceryList[0] << "\n";
+
+	//int oddNumbers[] = {1, 3, 5, 7};
+	//int bigBoy[10'000'000]; //how many Bytes is this? 
+
+	//cout << "What do it be? " << groceryList->at(4) << "\n";
+
+}
